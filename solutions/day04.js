741aed2cac1468a9753423ee8d263e671d4d5a94
Piotr Kaznowski <piotrkaznowski@gmail.com>
2020-11-30 00:25:43 +0100

    fixes day 4 part 2 broken by rogue refactor

--- solutions/day04.js
+++ solutions/day04.js
@@ -1,16 +1,14 @@
-const md5 = require('md5')
+const md5 = require("md5");
 
-const brutal = (input, start=1) => {
-  const secret = input.trim()
-  for (let i=start; i>0; i++) {
-    const hash = md5(`${secret}${i}`)
-    if (hash.slice(0, 5) == '00000') {
-      return i
-    }
+const brutal = (input, start = 1, matcher = "00000") => {
+  const secret = input.trim();
+  for (let i = start; i > 0; i++) {
+    const hash = md5(`${secret}${i}`);
+    if (hash.slice(0, matcher.length) == matcher) return i;
   }
-}
+};
 
 const part1 = (input) => { return brutal(input) };
-const part2 = (input) => { return brutal(input, 346386) };
+const part2 = (input) => { return brutal(input, 346386, "000000") };
 
 module.exports = [part1, part2];
