1c7cec2cbd401e9d2ee69b8c1bc09496109aac5a
Piotr Kaznowski <piotrkaznowski@gmail.com>
2020-12-09 23:16:20 +0100

    day 8 simplified

--- solutions/day08.js
+++ solutions/day08.js
@@ -6,14 +6,13 @@ const part1 = (input) =>
     .map((l) => l.length - eval(l).length)
     .reduce(h.add);
 
-const part2 = (input) => {
-  return h
+const part2 = (input) => 
+  h
     .asLines(input)
     .map((l) => {
       let [bs, qu] = [l.match(/\\/g), l.match(/"/g)];
       return (bs ? bs.length - (qu.length - 2) : 0) + 2 * qu.length;
     })
     .reduce(h.add);
-};
 
 module.exports = [part1, part2];
