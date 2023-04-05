4f155e1cec53fb4d32379c24ed8315c2b675a130
Piotr Kaznowski <piotr@kazno.dev>
2023-04-05 23:33:10 +0200

    Day 19.2 (todo: refactor to not use brute force)

--- Clojure/day19.clj
+++ Clojure/day19.clj
@@ -1,8 +1,9 @@
 (ns day19
   (:require [input :refer [f->str]]
-            [clojure.set :refer [union]]))
+            [clojure.set :refer [union difference]]))
 
-(defn parse [it]
+;; part 1
+(defn parse1 [it]
   (let [wx (re-seq #"\w+" it)]
     [(last wx)
      (->> wx butlast (partition 2) (reduce (fn [m [s r]] (update m (seq s) conj (seq r))) {}) vec)]))
@@ -16,7 +17,32 @@
              (inc n)
              (cond-> a (= c st) (into (for [r rs] (apply str (concat h r (drop cm t))))))))))))
 
+;; part 2, a brute solution (should be refactored to with analysis of common patterns in the input
+;; -- leaving for later :))
+
+(defn parse2 [it]
+  (let [wx (re-seq #"\w+" it)]
+    [(last wx)
+     (->> wx butlast (map seq) (partition 2) (map reverse))]))
+
+(defn replacements [m [st rs]]
+  (let [len (count m), cm (count st)]
+    (loop [n 0, a #{}]
+      (if (> n len) a
+          (let [[h t] ((juxt #(take n %) #(drop n %)) m), c (take cm t)]
+            (recur
+             (inc n)
+             (cond-> a (= c st) (conj (apply str (concat h rs (drop cm t)))))))))))
+
+(defn part2 [it rep]
+  (loop [[s n] [it 0], q (clojure.lang.PersistentQueue/EMPTY), seen #{it}]
+    (cond (= s "e") n
+          :else
+          (let [cx (vec (difference (apply union (for [r rep] (replacements s r))) seen))]
+            (if (empty? cx) (recur (peek q) (pop q) seen)
+                (recur [(first cx) (inc n)] (into q (map #(vector % (inc n)) cx)), (into seen cx)))))))
+
 (defn -main [day]
-  (let [[molecule replacements] (->> day f->str parse)]
-    {:part1 (count (apply union (for [r replacements] (replace-one molecule r))))
-     :part2 nil}))
+  (let [it (f->str day), [molecule replacements1] (parse1 it), [_ replacements2] (parse2 it)]
+    {:part1 (count (apply union (for [r replacements1] (replace-one molecule r))))
+     :part2 (part2 molecule replacements2)}))
